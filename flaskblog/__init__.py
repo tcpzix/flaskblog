b291f727e748a4093c9cf5f98c1543ee0ef3dab5
tcpzix <tcpzix@gmail.com>
2020-09-26 12:45:06 -0400

    add db and fresh project

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
 
 
 app = Flask(__name__)
+db_path = os.path.join(os.path.dirname(__file__), 'site.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 app.config['SECRET_KEY'] = '0312a2e2a0a4fcac8d99c4509a838'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -16,3 +20,8 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert'
 
 from flaskblog import routes
+
+
+
+
+
